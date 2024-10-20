afa15f64798ec1965f20757f61a40ecc89585a16
alphal <xiaodao_92@yeah.net>
2024-10-20 21:24:25 +0800

    feat: ✨ 新增 api 测试文件

---
+++ src/apis/__test__/graphql.ts
@@ -0,0 +1,45 @@
+import { grequest } from '@/apis/core/instance.ts';
+
+export const testGraphqlQuery = (variables = {}) => {
+  return grequest.query<{ id: number; name: string; remark: string }[]>({
+    query: `query queryEnvs {
+      envs {
+        id
+        name
+        remark
+      }
+    }
+  `,
+    variables,
+  });
+};
+
+export const testGraphqlMutation = (variables = {}) => {
+  return grequest.query<{ id: number; name: string; remark: string }[]>({
+    query: `mutation mutationEnvs {
+      envs {
+        id
+        name
+        remark
+      }
+    }
+  `,
+    variables,
+  });
+};
+
+export const testGraphqlSubscription = (config: any) => {
+  const { onMessage, variables = {} } = config;
+  return grequest.subscription({
+    query: `subscription subscriptionEnvs {
+      envs {
+        remark
+        name
+        id
+      }
+    }`,
+    operationName: 'subscriptionEnvs',
+    variables,
+    onMessage,
+  });
+};
