da91ffdef0ff7345e100d2e34b3e1e17c3164c07
alphal <xiaodao_92@yeah.net>
2024-10-21 17:07:46 +0800

    feat: ✨ 新增 pwa 启动配置

---
+++ src/utils/pwa.ts
@@ -0,0 +1,12 @@
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // show a prompt to user
+  },
+  onOfflineReady() {
+    // show a ready to work offline to user
+  },
+});
+
+updateSW();
