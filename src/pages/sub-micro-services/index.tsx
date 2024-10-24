34b1526bb9041f20d5f57f6e254ea364c43d98ab
alphal <xiaodao_92@yeah.net>
2024-10-24 10:07:19 +0800

    feat: ✨ 更新 微服务demo

--- src/pages/sub-micro-services/index.tsx
+++ src/pages/sub-micro-services/index.tsx
@@ -4,6 +4,11 @@ import { prefix } from './apps';
 import { useSubMicroServices } from '@/hooks/use-submicro-services.tsx';
 import { apps } from '@/microservices/apps.ts';
 
+const wrapStyle = {
+  width: '100%',
+  height: '100%',
+};
+
 export interface SubMicroServicesProps {
   [key: string]: any;
 }
@@ -12,12 +17,12 @@ const SubMicroServices: React.FC<SubMicroServicesProps> = (props: SubMicroServic
   const { error, microServiceApp } = useSubMicroServices({
     apps,
     prefix,
+    props: { foo: 'bar' },
   });
 
-  if (error)
-    return <div style={{ width: '100%', height: '100%' }}>load sub micro services error</div>;
+  if (error) return <div style={wrapStyle}>load sub micro services error</div>;
 
-  return <div style={{ width: '100%', height: '100%' }}>{microServiceApp()}</div>;
+  return <div style={wrapStyle}>{microServiceApp()}</div>;
 };
 
 export default React.memo(SubMicroServices);
