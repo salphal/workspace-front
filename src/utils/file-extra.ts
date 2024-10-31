5764bf4c41a65d4981ee6f2db4869fd2b716376b
alphal <xiaodao_92@yeah.net>
2024-10-31 17:57:15 +0800

    fix: 🐛 更新 jsdoc

--- src/utils/file-extra.ts
+++ src/utils/file-extra.ts
@@ -2,10 +2,11 @@ import { saveAs } from 'file-saver';
 
 export class FileExtra {
   /**
-   * @param {*} config
-   * @property {string} config.name - 文件名称
-   * @property {string} config.content - 文件内容
-   * @property {string} config.type - 文件类型
+   * 文件存储
+   * @param {*} config - 配置对象
+   * @param {string} config.name - 文件名称
+   * @param {string} config.content - 文件内容
+   * @param {string} config.type [application/octet-stream] - 文件类型
    */
   static saveAs(config: { name: string; content: BlobPart; type?: string }) {
     const { content, name, type = 'application/octet-stream' } = config;
