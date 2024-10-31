27610cb5928c04380ff3eb24d945f7a3443bc8a3
alphal <xiaodao_92@yeah.net>
2024-10-31 12:33:12 +0800

    feat: ✨ 添加 懒加载组件 loading 的定位

--- src/components/lazy-import-component.tsx
+++ src/components/lazy-import-component.tsx
@@ -1,12 +1,23 @@
 import React, { FC, LazyExoticComponent } from 'react';
 import { Spin } from 'antd';
 
+const Loading = () => (
+  <Spin
+    style={{
+      position: 'absolute',
+      top: '45%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+    }}
+  />
+);
+
 interface LazyImportComponentProps {
   lazyChildren: LazyExoticComponent<React.ComponentType<any>>;
 }
 
 const LazyImportComponent: FC<LazyImportComponentProps> = ({ lazyChildren: LazyChildren }) => (
-  <React.Suspense fallback={<Spin />}>
+  <React.Suspense fallback={<Loading />}>
     <LazyChildren />
   </React.Suspense>
 );
