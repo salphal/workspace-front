17f95d87bbf002dc2ac0c7f560787dec67b0f844
alphal <xiaodao_92@yeah.net>
2024-11-13 09:17:06 +0800

    feat: ✨ 添加文件类型注释

--- src/utils/file-extra.ts
+++ src/utils/file-extra.ts
@@ -7,6 +7,20 @@ export class FileExtra {
    * @param {string} config.name - 文件名称
    * @param {BlobPart} config.content - 文件内容
    * @param {string} [config.type=application/octet-stream] - 文件类型
+   *
+   * 常见文件类型：
+   * - text/plain: 普通文本文件
+   * - application/json: JSON 文件
+   * - text/html: HTML 文件
+   * - application/xml: XML 文件
+   * - image/jpeg: JPEG 图像
+   * - image/png: PNG 图像
+   * - application/pdf: PDF 文件
+   * - application/zip: ZIP 压缩文件
+   * - audio/mpeg: MP3 音频文件
+   * - video/mp4: MP4 视频文件
+   *
+   * 如果未提供 `type`，默认值为 `application/octet-stream`，表示二进制流文件类型。
    */
   static saveAs(config: { name: string; content: BlobPart; type?: string }) {
     const { content, name, type = 'application/octet-stream' } = config;
