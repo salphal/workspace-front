9d90e56a2db8ec07bd354b14596f3911e951e666
alphal <xiaodao_92@yeah.net>
2024-11-20 20:28:03 +0800

    feat: ✨ 更新 theme 工具方法

---
+++ src/utils/tailwind/theme.ts
@@ -0,0 +1,49 @@
+/**
+ * 主题集合
+ */
+export const themeKeys = {
+  dark: 'dark',
+  light: 'light',
+} as const;
+
+type ThemeName = keyof typeof themeKeys;
+
+/**
+ * 主题名称列表
+ */
+export const themeNameList: Array<ThemeName> = Object.keys(themeKeys) as Array<ThemeName>;
+
+/**
+ * 获取主题类名
+ * @param {ThemeName} name - 主题名称
+ */
+const getThemeName = (name: ThemeName) => `theme-${name}`;
+
+/**
+ * 根据名称切换主题
+ * @param {ThemeName} name - 主题名称
+ */
+export function toggleTheme(name: ThemeName) {
+  const root = document.documentElement;
+  const themeName = getThemeName(name);
+  // 匹配以 'theme-' 开头的类名
+  const themeClassRegex = /^theme-/;
+  // 查找并替换现有的主题类
+  root.classList.forEach((cls) => {
+    if (themeClassRegex.test(cls)) {
+      root.classList.replace(cls, themeName);
+    }
+  });
+  // 如果没有找到任何主题类，直接添加新的主题类
+  if (!root.classList.contains(themeName)) {
+    root.classList.add(themeName);
+  }
+}
+
+/**
+ * 初始化主题为 theme-light
+ */
+export const initTheme = () => {
+  const lightTheme = getThemeName(themeKeys.light);
+  document.documentElement.classList.add(lightTheme);
+};
