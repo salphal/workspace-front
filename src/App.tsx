bd3a33acc0c4dfe8450ae45c19d9eb3e5e796780
alphal <xiaodao_92@yeah.net>
2024-11-20 20:28:26 +0800

    feat: ✨ 测试工具类

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocation, useRoutes } from 'react-router-dom';
 
 import Layout from '@/layout';
@@ -6,12 +6,18 @@ import routes from '@/routes';
 
 import './App.scss';
 
+import { themeKeys, toggleTheme } from '@/utils/tailwind/theme.ts';
+
 function App() {
   const { pathname } = useLocation();
 
   const page = useRoutes(routes);
 
   if (['/overview'].includes(pathname)) return page;
+
+  useEffect(() => {
+    toggleTheme(themeKeys.dark);
+  }, []);
 
   return (
     <React.Fragment>
