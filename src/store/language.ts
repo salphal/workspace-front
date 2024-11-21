437cd00e8a167794d34c640680ba2ed3ba9624f2
alphal <xiaodao_92@yeah.net>
2024-11-21 14:57:44 +0800

    feat: ✨ 新增全局仓库组件

---
+++ src/store/language.ts
@@ -0,0 +1,67 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+
+import { setStoreProperties, zustandLocalStorage } from '@/utils/store/zustand.ts';
+
+/**
+ * https://ant-design.antgroup.com/docs/react/i18n-cn
+ */
+
+export interface ILanguageStore {
+  locale: any;
+}
+
+export const initialLanguage: ILanguageStore = {
+  locale: '',
+};
+
+// useShallow(); 对象浅比较, 减少重绘
+// const {
+//   locale,
+// } = useLanguageStore(useShallow((state: any) => state));
+const useLanguageStore = create(
+  persist<ILanguageStore>(
+    (set, get) => ({
+      ...initialLanguage,
+      // data: [],
+      // setData: () => set((state) => ({foo: "bar"})), // 将返回的对象与之前的对象合并
+      // getData: () => get().data.map((v: any) => !!v),
+    }),
+    {
+      name: 'languageStore', // unique name
+      version: 1,
+      storage: createJSONStorage(() => zustandLocalStorage), // zustandLocalStorage | zustandSessionStorage | localStorage ...
+    },
+  ),
+);
+
+export const setLanguageStore = (props: any) =>
+  useLanguageStore.setState((prev: any) => ({ ...prev, ...props }));
+
+export const setLanguageProperty = (
+  key: string,
+  value: any,
+  merge = false,
+  insertBefore = false,
+  isDeconstruct = true,
+) =>
+  setStoreProperties({
+    name: 'useLanguageStore',
+    store: useLanguageStore,
+    key,
+    value,
+    merge,
+    insertBefore,
+    isDeconstruct,
+  });
+
+export const setLocale = (value: any, merge = false, insertBefore = false, isDeconstruct = false) =>
+  setLanguageProperty('locale', value, merge, insertBefore, isDeconstruct);
+
+export const resetLocale = () => setLanguageProperty('locale', initialLanguage.locale, false);
+
+export const resetLanguageStore = () => useLanguageStore.setState({ ...initialLanguage });
+
+export const clearLanguageStoreStorage = () => useLanguageStore.persist.clearStorage();
+
+export default useLanguageStore;
---
+++ src/store/theme.ts
@@ -0,0 +1,74 @@
+import { ThemeConfig } from 'antd/es/config-provider/context';
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+
+import { setStoreProperties, zustandLocalStorage } from '@/utils/store/zustand.ts';
+
+/**
+ * https://ant-design.antgroup.com/docs/react/customize-theme-cn
+ */
+
+export interface IThemeStore {
+  mode: 'light' | 'dark' | 'auto';
+  theme: ThemeConfig;
+}
+
+export const initialTheme: IThemeStore = {
+  mode: 'light',
+  theme: {
+    token: {
+      // Seed Token，影响范围大
+    },
+  },
+};
+
+// useShallow(); 对象浅比较, 减少重绘
+// const {
+//   mode,
+// } = useThemeStore(useShallow((state: any) => state));
+const useThemeStore = create(
+  persist<IThemeStore>(
+    (set, get) => ({
+      ...initialTheme,
+      // data: [],
+      // setData: () => set((state) => ({foo: "bar"})), // 将返回的对象与之前的对象合并
+      // getData: () => get().data.map((v: any) => !!v),
+    }),
+    {
+      name: 'themeStore', // unique name
+      version: 1,
+      storage: createJSONStorage(() => zustandLocalStorage), // zustandLocalStorage | zustandSessionStorage | localStorage ...
+    },
+  ),
+);
+
+export const setThemeStore = (props: any) =>
+  useThemeStore.setState((prev: any) => ({ ...prev, ...props }));
+
+export const setThemeProperty = (
+  key: string,
+  value: any,
+  merge = false,
+  insertBefore = false,
+  isDeconstruct = true,
+) =>
+  setStoreProperties({
+    name: 'useThemeStore',
+    store: useThemeStore,
+    key,
+    value,
+    merge,
+    insertBefore,
+    isDeconstruct,
+  });
+
+export const setMode = (value: any, merge = false, insertBefore = false, isDeconstruct = false) =>
+  setThemeProperty('mode', value, merge, insertBefore, isDeconstruct);
+
+export const resetMode = () => setThemeProperty('mode', initialTheme.mode, false);
+
+export const resetThemeStore = () => useThemeStore.setState({ ...initialTheme });
+
+export const clearThemeStoreStorage = () => useThemeStore.persist.clearStorage();
+
+export default useThemeStore;
