bb321ca53a12ae44c04e0f1e0dbfd871ad8a2350
alphal <xiaodao_92@yeah.net>
2024-11-25 16:18:22 +0800

    test: ✅ 更新测试代码

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import { Segmented, Typography } from 'antd';
 
-import styles from './test.module.scss';
-import { testGraphqlSubscription } from '@/apis/__test__/graphql.ts';
-
-const { Title, Paragraph, Text, Link } = Typography;
+import CodeEditor from '@/components/code-editor';
 
 export interface TestProps {
   [key: string]: any;
@@ -13,77 +9,15 @@ export interface TestProps {
 const Test: React.FC<TestProps> = (props: TestProps) => {
   useEffect(() => {}, []);
 
-  // useEffect(() => {
-  //   testGraphqlQuery().then((res: any) => {
-  //     const { error, data } = res;
-  //     if (!error) {
-  //       console.log('=>(test.tsx:18) data1', data);
-  //     }
-  //   });
-  // }, []);
-
-  const [segmented, setSegmented] = useState<string>('testGraphqlQuery');
-
-  const [data, setData] = useState([]);
-
-  const socketRef = useRef<any>();
-
-  useEffect(() => {
-    testGraphqlSubscription({ onMessage: dataOnMessage }).then((ws) => {
-      socketRef.current = ws;
-    });
-    return () => {
-      typeof socketRef.current.disconnect === 'function' && socketRef.current.disconnect();
-    };
-  }, []);
-
-  const dataOnMessage = (data: any) => {
-    if (Array.isArray(data.envs)) {
-      setData(data.envs);
-    }
-  };
+  const value = `
+    <React.Fragment>
+      <CodeEditor value={value} onChange={() => {}} language={'javascript'} />
+    </React.Fragment>;
+  `;
 
   return (
     <React.Fragment>
-      <Segmented<string>
-        value={segmented}
-        onChange={(value) => {
-          setSegmented(value);
-        }}
-        options={[
-          {
-            label: '测试 graphql 查询',
-            value: 'testGraphqlQuery',
-          },
-          {
-            label: '测试 graphql 修改',
-            value: 'testGraphqlMutation',
-          },
-          {
-            label: '测试 graphql 订阅',
-            value: 'testGraphqlSubscription',
-          },
-        ]}
-        size={'large'}
-        block
-      />
-      <Typography className={styles.content}>
-        {segmented === 'testGraphqlQuery' && (
-          <>
-            <Paragraph>testGraphqlQuery</Paragraph>
-          </>
-        )}
-        {segmented === 'testGraphqlMutation' && (
-          <>
-            <Paragraph>testGraphqlMutation</Paragraph>
-          </>
-        )}
-        {segmented === 'testGraphqlSubscription' && (
-          <>
-            <Paragraph>{JSON.stringify(data)}</Paragraph>
-          </>
-        )}
-      </Typography>
+      <CodeEditor value={value} onChange={() => {}} language={'javascript'} />
     </React.Fragment>
   );
 };
