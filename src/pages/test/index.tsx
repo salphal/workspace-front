ae1968c515b39f11b0e08155b8e46c7ec16600d7
alphal <xiaodao_92@yeah.net>
2025-11-16 23:19:04 +0800

    feat: ✨ update test page

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,15 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import './index.scss';
 
-export interface TestProps {
-  [key: string]: any;
-}
-
-const Index: React.FC<TestProps> = (props: TestProps) => {
-  useEffect(() => {}, []);
-
+const Test = () => {
   return <div className={'test-wrap'}>hello world</div>;
 };
 
-export default React.memo(Index);
+export default Test;
